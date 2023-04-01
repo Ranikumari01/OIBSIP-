53b94c8fc92814c1d55b8ec3247e81ea0e58c17c
Rani Kumari <97820654+Ranikumari01@users.noreply.github.com>
2023-04-01 11:59:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+let display = document.getElementById('display');
+let buttons = Array.from(document.getElementsByClassName('button'));
+buttons.map(button =>{
+    button.addEventListener('click',(e)=>{
+        switch(e.target.innerText)
+        {
+            case 'c':
+                display.innerText='';
+                break;
+
+            case '←':
+                if(display.innerText){
+                display.innerText = display.innerText.slice(0,-1);
+                 }
+                  break;
+
+                case '=':
+                      try{
+                        display.innerText = eval(display.innerHTML);  
+                    }
+             catch
+           {
+            display.innerText='error' ;
+           }
+                    break;
+
+
+                default:
+                    display.innerText += e.target.innerText;
+        }
+
+    });
+});
